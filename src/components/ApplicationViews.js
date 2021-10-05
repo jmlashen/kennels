0fe6263fc672be47decb4bf7ff2484c50b8a2ccf
Jake Lashenik <jmlashen@gmail.com>
2021-10-04 22:17:33 -0500

    created animal with form. simple authentication and conditional routing. edit animal detail.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalList } from "./animal/AnimalList"
 import { CustomerList } from "./customer/CustomerList"
@@ -7,8 +7,24 @@ import { EmployeeList } from "./employee/EmployeeList"
 import { LocationList } from "./location/LocationList"
 import { AnimalDetail } from "./animal/AnimalDetail"
 import { EmployeeDetail } from "./employee/EmployeeDetail"
+import { AnimalForm } from './animal/AnimalForm'
+import { Login } from "./auth/Login"
+import { useState } from "react"
+import { Register } from "./auth/Register"
+import { CustomerDetail } from "./customer/CustomerDetail"
+import { LocationDetail } from "./location/LocationDetail"
+import { AnimalEditForm } from "./animal/AnimalEditForm"
+
 
 export const ApplicationViews = ({ isAdmin, myUser }) => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("kennel_customer") !== null)
+
+  const setAuthUser = (user) => {
+    sessionStorage.setItem("kennel_customer", JSON.stringify(user))
+    setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+  }
+
   return (
     <>
       {/* Render the location list when http://localhost:3000/ */}
@@ -18,8 +34,19 @@ export const ApplicationViews = ({ isAdmin, myUser }) => {
 
       {/* Render the animal list when http://localhost:3000/animals */}
       <Route exact path="/animals">
-        <AnimalList />
+        { isAuthenticated ? <AnimalList /> : <Redirect to="/login" />}
       </Route>
+
+      <Route exact path="/animals/:animalId(\d+)/edit">
+        {isAuthenticated ? <AnimalEditForm /> :
+        
+      <Redirect to="/login" /> }
+            
+      </Route>
+
+        
+
+
 
       {/* Render the animal list when http://localhost:3000/customers */}
       <Route exact path="/customers">
@@ -40,9 +67,31 @@ export const ApplicationViews = ({ isAdmin, myUser }) => {
         <AnimalDetail />
       </Route>
 
+      <Route path="/customers/:customerId(\d+)">
+        <CustomerDetail />
+      </Route>
+
+      <Route path="/locations/:locationId(\d+)">
+        <LocationDetail />
+      </Route>
+
       <Route path="/employees/:employeeId(\d+)">
         <EmployeeDetail />
       </Route>
+
+
+      <Route path="/animals/create">
+        <AnimalForm />
+      </Route>
+
+      <Route path="/login">
+        <Login setAuthUser={setAuthUser} />
+      </Route>
+
+      <Route path="/register">
+        <Register setAuthUser={setAuthUser} />
+      </Route>
+
 
       {/*
   This is a new route to handle a URL with the following pattern:
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { getAllAnimals } from '../../modules/AnimalManager';
 import { AnimalCard } from './AnimalCard';
 import { deleteAnimal } from '../../modules/AnimalManager';
+import { useHistory } from 'react-router';
 
 export const AnimalList = () => {
-    //this is invoked
-    const [animals, setAnimals] = useState([]);
+  //this is invoked
+  const [animals, setAnimals] = useState([]);
+  const history = useHistory();
   const GetAnimals = () => { //this function gets all the animals from the animal manager. this allows to call getAnimals anytime so we dont have to use useEffect everytime
+
     
+
+
     //      varible, function
     return getAllAnimals().then(animalsFromAPI => {
       // We'll do something more interesting with this data soon.
@@ -20,30 +25,44 @@ export const AnimalList = () => {
 
   const handleDeleteAnimal = id => {
     deleteAnimal(id)
-    .then(() => getAllAnimals().then(setAnimals));
-};
+      .then(() => getAllAnimals().then(setAnimals));
+  };
 
 
   useEffect(() => { //this is number 4 
-      //happens after the return/fetch data from the database and changes the state. on load of this component what do I need to happen. If I need to get data, it will only get that data on this first load. it wont continulously get data. 
+    //happens after the return/fetch data from the database and changes the state. on load of this component what do I need to happen. If I need to get data, it will only get that data on this first load. it wont continulously get data. 
 
     GetAnimals(); //this changes the state and then re-renders
   }, []); //empty array only runs after the first render
 
-    // Finally we use .map() to "loop over" the animals array to show a list of animal cards
   // Finally we use .map() to "loop over" the animals array to show a list of animal cards
-//   return (
-//     <div className="container-cards">
-//       {animals.map(animal => <AnimalCard key={animal.id} animal={animal} handleDeleteAnimal={handleDeleteAnimal}/>)}
-//     </div>
-//   );
-// };
+  // Finally we use .map() to "loop over" the animals array to show a list of animal cards
+  //   return (
+  //     <div className="container-cards">
+  //       {animals.map(animal => <AnimalCard key={animal.id} animal={animal} handleDeleteAnimal={handleDeleteAnimal}/>)}
+  //     </div>
+  //   );
+  // };
 
-// export const AnimalList = () => {
+  // export const AnimalList = () => {
 
   // ...code omitted for brevity...
 
   return (
+    //add this button above your display of animal cards
+<>
+<section className="section-content">
+  
+  <button type="button"
+      className="btn"
+      onClick={() => {history.push("/animals/create")}}>
+      Admit Animal
+  </button>
+
+</section>
+
+
+
     <div className="container-cards">
       {animals.map(animal =>
         <AnimalCard
@@ -51,6 +70,7 @@ export const AnimalList = () => {
           animal={animal}
           handleDeleteAnimal={handleDeleteAnimal} />)}
     </div>
+  </>
   );
 };
 
